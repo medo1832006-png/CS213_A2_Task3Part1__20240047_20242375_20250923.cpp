48be1c3be18c5f8f2e849289d6a3bb2300f72858
ahmedmo565 <ahmedmsaeed172@gmail.com>
2025-10-23 20:31:55 +0300

    add your part here

--- MainComponent.h
+++ MainComponent.h
@@ -1,44 +1,20 @@
-
 #pragma once
-
 #include <JuceHeader.h>
-#include <vector>
+#include "PlayerGUI.h"
 
-class MainComponent : public juce::AudioAppComponent,
-                      public juce::Button::Listener,
-                      public juce::Slider::Listener
+class MainComponent : public juce::AudioAppComponent
 {
 public:
     MainComponent();
     ~MainComponent() override;
 
-    // Audio
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
-
-    // GUI
-    void paint(juce::Graphics& g) override;
     void resized() override;
 
-    // Event handlers
-    void buttonClicked(juce::Button* button) override;
-    void sliderValueChanged(juce::Slider* slider) override;
-
 private:
-    // Audio
-    juce::AudioFormatManager formatManager;
-    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
-    juce::AudioTransportSource transportSource;
-
-    // GUI Controls
-    juce::TextButton loadButton { "Load Files" };
-    juce::TextButton restartButton { "Restart" };
-    juce::TextButton stopButton { "Stop" };
-    juce::Slider volumeSlider;
-
-    //void loadTrack(const juce::File& file);
-    std::unique_ptr<juce::FileChooser> fileChooser;
+    PlayerGUI player;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
